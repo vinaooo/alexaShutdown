ffacb0018596c2751d37db9dbf64c71bfcc26d6d
Vinicius R Pedrinho <vrpedrinho@gmail.com>
2023-08-19 14:41:17 -0300

    modified:   alexaDesliga.py otimized code

--- alexaDesliga.py
+++ alexaDesliga.py
@@ -1,70 +1,91 @@
-#vinicius
-
-import gi
+import os
 import subprocess
-import os   
+import gi
 
-gi.require_version('Gtk', '3.0')
+gi.require_version("Gtk", "4.0")
 from gi.repository import Gtk, GLib
 
-class CenteredTextWindow(Gtk.Window):
+def on_destroy(window):
+    subprocess.run(["shutdown", "-c"])                                  # Cancel shutdown on close window
+
+class CentralizedTextApp(Gtk.Application):
     def __init__(self):
-        Gtk.Window.__init__(self, title="Desligar PC con Alexa")
-        self.set_default_size(300, 200)                 #screen size
-        self.set_resizable(False)                       #screen size fixed
-        self.set_position(Gtk.WindowPosition.CENTER)    #screem position
+        super().__init__(
+            application_id="com.example.centralized_text_app")
 
-        main_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
-        self.add(main_box)
+        subprocess.run(["shutdown", "+2"])                              # Shutdown in 2 minutes
 
-        self.label = Gtk.Label()
-        main_box.pack_start(self.label, True, True, 0)
+        self.counter = 120
+        self.timer_id = None
+        self.label = None
 
-        self.label.set_halign(Gtk.Align.CENTER)         #horizontal alignment to center
-        self.label.set_valign(Gtk.Align.CENTER)         #vertical alignment to center
+    def do_activate(self):
+        self.timer_id = GLib.timeout_add(1000, self.update_counter)     # Start the timer
 
-        button_box = Gtk.Box(spacing=6)
-        main_box.pack_start(button_box, False, False, 0)
+        window = Gtk.ApplicationWindow(application=self)
+        window.set_title("Alexa Shutdown pc")
+        window.set_default_size(350, 150)                               # Set the size of the window
+        window.set_resizable(False)                                     # Fixes the window from being resized
 
-        button_box.set_halign(Gtk.Align.END)
-        button_box.set_hexpand(True)
+        def on_cancel_clicked(button):
+            GLib.source_remove(self.timer_id)
+            self.label.set_label("Shutdown aborted")
 
-        btnCancelar = Gtk.Button(label="Cancelar")
-        btnCancelar.connect("clicked", self.on_btnCancelar_clicked)
-        btnCancelar.set_size_request(100, 30)                       #button size
-        btnCancelar.set_margin_bottom(10)                           #button margin
-        button_box.pack_start(btnCancelar, False, False, 0)         #button position
+            subprocess.run(["shutdown", "-c"])                          # Cancel shutdown
+            button.set_sensitive(False)  
 
-        btnDesligar = Gtk.Button(label="Desligar")
-        btnDesligar.connect("clicked", self.on_btnDesligar_clicked)
-        btnDesligar.set_size_request(100, 30)                       #button size
-        btnDesligar.set_margin_end(10)                              #button margin
-        btnDesligar.set_margin_bottom(10)                           #button margin
-        button_box.pack_start(btnDesligar, False, False, 0)         #button position
+        box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+        window.set_child(box)                                           
 
-        subprocess.run(["shutdown", "+2"])                          #shutdown in 2 minutes
-        self.counter = 120                                          #time to shutdown   
-        self.update_counter()
-        self.timeout_id = None                                      #Initialize the timeout ID
+        self.label = Gtk.Label(label=" ")
+        self.label.set_margin_top(50)
+        self.label.set_margin_bottom(50)
+        self.label.set_halign(Gtk.Align.CENTER)
+        self.label.set_valign(Gtk.Align.CENTER)
+        box.append(self.label)
+
+        button_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
+        button_box.set_halign(Gtk.Align.CENTER)
+        button_box.set_margin_bottom(10)
+
+        box.append(button_box)
+
+        btn_cancel = Gtk.Button(label="Cancel")
+        btn_cancel.set_size_request(100, -1)
+        button_box.append(btn_cancel)
+
+        btn_Shutdown = Gtk.Button(label="Shutdown")
+        btn_Shutdown.set_size_request(100, -1)
+        button_box.append(btn_Shutdown)
+
+        def update_counter():
+            minutes = self.counter // 60
+            seconds = self.counter % 60
+            self.label.set_label("Shutting pc down in " + str(minutes) + " minutes and " + str(seconds) + " seconds")
+            self.counter -= 1
+            if self.counter == 0:
+                GLib.source_remove(self.timer_id)
+            return True
+
+
+        def on_shutdown_clicked(button):
+            os.system('systemctl poweroff')                             # Force shutdown instantly
+
+        btn_cancel.connect("clicked", on_cancel_clicked)
+        btn_Shutdown.connect("clicked", on_shutdown_clicked)
+
+        window.connect("destroy", on_destroy)                           # Connect the window destroy to the on_destroy
+
+        window.set_visible(True)
 
     def update_counter(self):
-        self.label.set_text(f"Desligando computador em {self.counter} segundos!")
+        minutes = self.counter // 60
+        seconds = self.counter % 60
+        self.label.set_label("Shutting pc down in " + str(minutes) + " minutes and " + str(seconds) + " seconds")
+        self.counter -= 1
+        if self.counter == 0:
+            GLib.source_remove(self.timer_id)
+        return True
 
-        if self.counter > 0:
-            self.counter -= 1
-            self.timeout_id = GLib.timeout_add_seconds(1, self.update_counter)
-
-    def on_btnCancelar_clicked(self, button):
-        subprocess.run(["shutdown", "-c"])          #Cancel shutdown
-        subprocess.run(["shutdown", "--show"])      #Show shutdown schedule
-        if self.timeout_id is not None:
-            GLib.source_remove(self.timeout_id)
-            self.timeout_id = None                  #Reset the timeout ID
-
-    def on_btnDesligar_clicked(self, button):
-        os.system('systemctl poweroff')             #Shutdown
-
-win = CenteredTextWindow()
-win.connect("destroy", Gtk.main_quit)
-win.show_all()
-Gtk.main()
+app = CentralizedTextApp()
+app.run(None)
